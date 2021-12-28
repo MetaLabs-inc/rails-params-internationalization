fab522587c3208bbf2080eca1c148af8a233eb25
Marco Fiorito <marcofiorito1@gmail.com>
2021-12-27 23:48:59 -0300

    first commit

---
+++ lib/rails/params/internationalization.rb
@@ -0,0 +1,10 @@
+require "rails/params/internationalization/version"
+
+module Rails
+  module Params
+    module Internationalization
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/params/internationalization/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Params
+    module Internationalization
+      VERSION = "0.1.0"
+    end
+  end
+end
